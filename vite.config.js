ada62b4f60f67cae3f2dea78814e6cea78b13639
Carlos <c.pena17@ufromail.cl>
2025-05-20 13:49:46 -0400

    fix: update base URL in Vite configuration

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: 'https://carlosesteban-afk.github.io/pwa-test/', 
   plugins: [vue(), VitePWA({
     registerType: 'autoUpdate',
     injectRegister: false,
