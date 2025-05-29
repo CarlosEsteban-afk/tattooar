ca426679f6652b0af47324dd90a6c5311a9e0ac3
CARLOS FERNANDO ITURRA BRAVO <c.iturra07@ufromail.cl>
2025-05-29 03:20:24 -0400

    fix: fixed url hash problem

--- src/router/router.js
+++ src/router/router.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import RegisterView from "../views/login/RegisterView.vue";
 import LoginView from "../views/login/LoginView.vue";
 import ForgotPasswordView from "../views/login/ForgotPasswordView.vue";
@@ -48,7 +48,7 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHashHistory("/tattooar/"),
+  history: createWebHistory("/tattooar/"),
   routes,
 });
 
